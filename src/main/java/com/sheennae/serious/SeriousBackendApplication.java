9694fb913cd58478605a2a7d83e6815459499dbc
gksxodnd007 <gksxodnd007@gmail.com>
2017-11-04 21:32:10 +0900

    first commit

---
+++ src/main/java/com/sheennae/serious/SeriousBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sheennae.serious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriousBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriousBackendApplication.class, args);
+	}
+}
