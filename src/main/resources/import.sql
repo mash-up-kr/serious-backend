d12867f19c80474e4383f8cd66f098532b03e716
gksxodnd007 <gksxodnd007@gmail.com>
2017-11-06 23:29:03 +0900

    test

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,5 @@ INSERT INTO user_color (type, code) VALUES ('LEFT', '#FF530D');
 INSERT INTO user_color (type, code) VALUES ('MID', '#25303F');
 INSERT INTO user_color (type, code) VALUES ('RIGHT', '#00BEFF');
 INSERT INTO user_color (type, code) VALUES ('EXTREME_RIGHT', '#0053FF');
+
+INSERT INTO subject (title, create_at) VALUES ('노룩패스 실화인가', now());
