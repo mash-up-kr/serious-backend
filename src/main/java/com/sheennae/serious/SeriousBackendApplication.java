a63092d616e3e40fc88451616a1cdc4babde0c70
gksxodnd007 <gksxodnd007@gmail.com>
2017-11-06 23:32:55 +0900

    test

--- src/main/java/com/sheennae/serious/SeriousBackendApplication.java
+++ src/main/java/com/sheennae/serious/SeriousBackendApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 @EntityScan(
 		basePackageClasses = { Jsr310JpaConverters.class }, basePackages = {"com.sheennae.serious.model"})
 public class SeriousBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SeriousBackendApplication.class, args);
 	}
