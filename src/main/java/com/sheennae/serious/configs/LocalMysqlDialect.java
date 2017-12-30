8ce1a3190392cc086a7ed3a108d7c61ca05c2228
bigstark <kkjkkdk@gmail.com>
2017-12-30 21:10:04 +0900

    utf8 -> utf8mb4 in local mysql dialect

--- src/main/java/com/sheennae/serious/configs/LocalMysqlDialect.java
+++ src/main/java/com/sheennae/serious/configs/LocalMysqlDialect.java
@@ -5,6 +5,6 @@ import org.hibernate.dialect.MySQL5InnoDBDialect;
 public class LocalMysqlDialect extends MySQL5InnoDBDialect {
     @Override
     public String getTableTypeString() {
-        return " ENGINE=InnoDB DEFAULT CHARSET=utf8";
+        return " ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
     }
 }
