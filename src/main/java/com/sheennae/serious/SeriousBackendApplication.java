0f36e0df90fdca1ffb5e21a37ee45381339f7645
bigstark <kkjkkdk@gmail.com>
2018-01-05 14:56:27 +0900

    add annotations

--- src/main/java/com/sheennae/serious/SeriousBackendApplication.java
+++ src/main/java/com/sheennae/serious/SeriousBackendApplication.java
@@ -1,10 +1,13 @@
 package com.sheennae.serious;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,6 +16,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.sheennae.serious")
+@ComponentScan
+@EnableAutoConfiguration
+@Configuration
 @EntityScan(
 		basePackageClasses = { Jsr310JpaConverters.class }, basePackages = {"com.sheennae.serious.model"})
 public class SeriousBackendApplication extends ServletInitializer {
